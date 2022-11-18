d992790d5b2ec26dcc5491339a0237b7df285704
Pexxed <47058879+Pexxed@users.noreply.github.com>
2022-11-18 14:08:40 +0100

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,16 @@
+package first;
+
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int erg = getCalculation(100, 1000);
+        System.out.println(erg);
+
+    }
+
+    private static int getCalculation(int i, int j) {
+        return i + j;
+    }
+}
+
+
